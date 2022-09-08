1ee17caebb4e74c5c25640331ad28a2a571443ca
Ajay Mathew <68919307+ajay007e@users.noreply.github.com>
2022-09-08 21:53:23 +0530

    Day 7 updated

--- day 7/main.py
+++ day 7/main.py
@@ -1,3 +1,4 @@
 # there is no coding exercise available in day 7
 # only theoretical classes are available in day 7
 # unable to do coding today also
+# 4 days of no coding ,will be compensated soon
