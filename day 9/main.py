eb8ece92fc752873084755c0a567d3ea02cde5fc
Ajay Mathew <ajayedathazhe@gmail.com>
2022-09-13 23:46:40 +0530

    day 9 completed

---
+++ day 9/main.py
@@ -0,0 +1,58 @@
+student_scores = {
+    "Harry": 81,
+    "Ron": 78,
+    "Hermione": 99,
+    "Draco": 74,
+    "Neville": 62,
+}
+# 🚨 Don't change the code above 👆
+
+#TODO-1: Create an empty dictionary called student_grades.
+student_grades = {}
+
+#TODO-2: Write your code below to add the grades to student_grades.👇
+for key in student_scores:
+    if int(student_scores[key]) > 90:
+        student_grades[key] = "Outstanding"
+    elif int(student_scores[key]) > 80:
+        student_grades[key] = "Exceeds Expectations"
+    elif int(student_scores[key]) > 70:
+        student_grades[key] = "Acceptable"
+    else:
+        student_grades[key] = "Fail"
+
+# 🚨 Don't change the code below 👇
+print(student_grades)
+
+# ------------------------------------------------------------------------------------------------------------------------
+
+travel_log = [
+{
+  "country": "France",
+  "visits": 12,
+  "cities": ["Paris", "Lille", "Dijon"]
+},
+{
+  "country": "Germany",
+  "visits": 5,
+  "cities": ["Berlin", "Hamburg", "Stuttgart"]
+},
+]
+#🚨 Do NOT change the code above
+
+#TODO: Write the function that will allow new countries
+#to be added to the travel_log. 👇
+def add_new_country(country,visits,cities):
+  dic = {
+    "country" : country,
+    "visits" : visits,
+    "cities" : cities
+  }
+  travel_log.append(dic)
+
+#🚨 Do not change the code below
+add_new_country("Russia", 2, ["Moscow", "Saint Petersburg"])
+print(travel_log)
+
+
+
