d16b004f9fbd0ac010f3cefc3aa051ab6370020f
Ajay Mathew <ajayedathazhe@gmail.com>
2022-10-09 11:05:39 +0530

    day 25 csv file reading using pandas package

--- day 025/main.py
+++ day 025/main.py
@@ -1,10 +1,19 @@
-import csv
+import pandas as pd
 
-with open("day 025/weather_data.csv") as file:
-    data = csv.reader(file)
-    temp = []
-    for row in data:
-        print(row)
-        if row[1] != "temp":
-            temp.append(row[1])
-    print(temp)
+# import csv
+# with open("day 025/weather_data.csv") as file:
+#     data = csv.reader(file)
+#     temp = []
+#     for row in data:
+#         print(row)
+#         if row[1] != "temp":
+#             temp.append(row[1])
+#     print(temp)                                       // task 1 : csv reading using csv package
+
+
+
+data = pd.read_csv("day 025/weather_data.csv")
+print(data)
+print(data["temp"])                                   
+
+
