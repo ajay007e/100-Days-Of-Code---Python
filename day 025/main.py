7cf24bb9ea338207d4ac0d52d70a69dd80ca4dec
Ajay Mathew <ajayedathazhe@gmail.com>
2022-10-09 11:44:52 +0530

    day 25 squirrel data analysis

--- day 025/main.py
+++ day 025/main.py
@@ -18,33 +18,45 @@ import pandas as pd
 
 
 
-data = pd.read_csv("day 025/weather_data.csv")
+# data = pd.read_csv("day 025/weather_data.csv")
 
-## dataframe into dictionary
-data_dict = data.to_dict()
-print(data_dict)
+# ## dataframe into dictionary
+# data_dict = data.to_dict()
+# print(data_dict)
 
-## series into list
-data_list = data["temp"].to_list()
-print(data_list)
+# ## series into list
+# data_list = data["temp"].to_list()
+# print(data_list)
 
-## row selection using the pandas package
-print(data.temp)
+# ## row selection using the pandas package
+# print(data.temp)
 
-## methods in the pandas package
-print(data["temp"].mean())
+# ## methods in the pandas package
+# print(data["temp"].mean())
 
-## column selection using pandas package
-print(data[data.day == "Monday"])
-print(data[data.temp == data.temp.max() ])
+# ## column selection using pandas package
+# print(data[data.day == "Monday"])
+# print(data[data.temp == data.temp.max() ])
 
-## creating dataframe using pandas package
-data_dic = {
-    "students":["Ajay", "Amal", "Praveen"],
-    "scores":[20,30,40]
+# ## creating dataframe using pandas package
+# data_dic = {
+#     "students":["Ajay", "Amal", "Praveen"],
+#     "scores":[20,30,40]
+# }
+# data = pd.DataFrame(data_dic)
+
+# ### to create csv from dataframe
+# data.to_csv("day 025/new_csv.csv")                    // pandas package familarization
+
+
+
+data = pd.read_csv("day 025/Squirrel_Data.csv")
+black = data[data["Primary Fur Color"] == "Black"].index
+gray = data[data["Primary Fur Color"] == "Gray"].index
+cinnamon = data[data["Primary Fur Color"] == "Cinnamon"].index
+
+dic = {
+    "fur_color": ["Black","Gray","Cinnamon"],
+    "count":[len(black),len(gray),len(cinnamon)]
 }
-data = pd.DataFrame(data_dic)
-
-### to create csv from dataframe
-data.to_csv("day 025/new_csv.csv")
-
+pd.DataFrame(dic).to_csv("day 025/fur_data.csv")
