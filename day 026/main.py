a52ed3bd160c6e4ada1e4dab6a9171c5ac1667a9
Ajay Mathew <ajayedathazhe@gmail.com>
2022-10-11 21:56:24 +0530

    day 26 tasks updated

--- day 026/main.py
+++ day 026/main.py
@@ -49,3 +49,26 @@ def dicNumberOfLetters():
 
     print(result)
 
+
+def temperatureConversion():
+    weather_c = {
+        "Monday": 12,
+        "Tuesday": 14,
+        "Wednesday": 15,
+        "Thursday": 14,
+        "Friday": 21,
+        "Saturday": 22,
+        "Sunday": 24,
+    }
+    # 🚨 Don't change code above 👆
+
+
+    # Write your code 👇 below:
+
+    def fahenheit(temp):
+        return (temp * 9/5) + 32
+
+    weather_f = {day:fahenheit(value) for (day, value) in weather_c.items()}
+    print(weather_f)
+
+
