5cd0e76ca84db7480341c270c9821a6fb1e5ddf4
Ajay Mathew <ajayedathazhe@gmail.com>
2022-10-14 23:07:58 +0530

    day 27 completed

--- day 027/main.py
+++ day 027/main.py
@@ -1,10 +1,32 @@
 import tkinter
 
+def converter():
+    value = float(miles_entry.get())
+    value *=1.609
+    result.config(text=f"{value}")
+
 window = tkinter.Tk()
-window.title("GUI Program")
-window.minsize(500,300)
-new_label = tkinter.Label(text="New label",font=('Arial',15,'bold'))
-new_label.pack(expand=True)
+window.title("Miles to Km Converter")
+window.config(padx=20,pady=20)
+# window.minsize(500,300)
+
+miles_entry = tkinter.Entry(width=8)
+miles_entry.grid(column=0,row=0)
+
+is_eq = tkinter.Label(text="Is",font=('Arial',15,'normal'),)
+is_eq.grid(column=2,row=0)
+
+km = tkinter.Label(text="Kms",font=('Arial',15,'normal'))
+km.grid(column=4,row=0)
+
+miles = tkinter.Label(text="Miles",font=('Arial',15,'normal'))
+miles.grid(column=1,row=0)
+
+result = tkinter.Label(text="0",font=('Arial',15,'bold'))
+result.grid(column=3,row=0)
+
+submit = tkinter.Button(text="Calculate",command=converter)
+submit.grid(column=1,row=1,columnspan=3)
 
 
 
