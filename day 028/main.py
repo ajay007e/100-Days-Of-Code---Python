067fbdd9753f7e0ae1a4f153ea291845ef26df18
Ajay Mathew <ajayedathazhe@gmail.com>
2022-10-15 20:16:07 +0530

    day 28 pomodo timer functionality created

--- day 028/main.py
+++ day 028/main.py
@@ -16,9 +16,19 @@ TICK = "✓"
 
 # ---------------------------- TIMER MECHANISM ------------------------------- # 
 
+def start_timer():
+    count_down(60*WORK_MIN)
+
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
 
+def count_down(count):
+
+    canvas.itemconfig(timer_text,text=f"{count//60}:{str(count%60).zfill(2)}")
+    if count > 0 :
+        window.after(1000,count_down,count-1)
+
 # ---------------------------- UI SETUP ------------------------------- #
+
 window = tkinter.Tk()
 window.title("Pomodoro")
 window.config(padx=100,pady=50,bg=YELLOW)
@@ -29,10 +39,10 @@ timer_label.grid(column=1,row=0)
 canvas = tkinter.Canvas(width=206,height=224,bg=YELLOW,highlightthickness=0)
 tomato_img = tkinter.PhotoImage(file="day 028/tomato.png")
 canvas.create_image(100,112,image=tomato_img)
-canvas.create_text(100,140,text="00.00",fill="white",font=(FONT_NAME,30,"bold"))
+timer_text = canvas.create_text(100,140,text="00.00",fill="white",font=(FONT_NAME,30,"bold"))
 canvas.grid(column=1,row=1)
 
-start_btn = tkinter.Button(text="Start")
+start_btn = tkinter.Button(text="Start",command=start_timer)
 start_btn.grid(column=0,row=2)
 
 reset_btn = tkinter.Button(text="Reset")
@@ -40,10 +50,5 @@ reset_btn.grid(column=3,row=2)
 
 check_label = tkinter.Label(text=TICK,fg=GREEN,bg=YELLOW,font=(FONT_NAME,30,"normal"))
 check_label.grid(column=1,row=3)
-
-
-
-
-
 
 window.mainloop()
