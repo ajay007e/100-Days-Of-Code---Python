f3c36ff04eaabb6f16af8891dc62b7b9f8c1b991
Ajay Mathew <ajayedathazhe@gmail.com>
2022-10-15 20:01:38 +0530

    day 28 pomodoro ui completed

--- day 028/main.py
+++ day 028/main.py
@@ -1,4 +1,5 @@
 import tkinter
+from tkinter import font
 
 # ---------------------------- CONSTANTS ------------------------------- #
 PINK = "#e2979c"
@@ -9,6 +10,7 @@ FONT_NAME = "Courier"
 WORK_MIN = 25
 SHORT_BREAK_MIN = 5
 LONG_BREAK_MIN = 20
+TICK = "✓"
 
 # ---------------------------- TIMER RESET ------------------------------- # 
 
@@ -21,11 +23,25 @@ window = tkinter.Tk()
 window.title("Pomodoro")
 window.config(padx=100,pady=50,bg=YELLOW)
 
+timer_label = tkinter.Label(text="Timer",fg=GREEN,bg=YELLOW,font=(FONT_NAME,45,"bold"))
+timer_label.grid(column=1,row=0)
+
 canvas = tkinter.Canvas(width=206,height=224,bg=YELLOW,highlightthickness=0)
 tomato_img = tkinter.PhotoImage(file="day 028/tomato.png")
 canvas.create_image(100,112,image=tomato_img)
 canvas.create_text(100,140,text="00.00",fill="white",font=(FONT_NAME,30,"bold"))
-canvas.pack()
+canvas.grid(column=1,row=1)
+
+start_btn = tkinter.Button(text="Start")
+start_btn.grid(column=0,row=2)
+
+reset_btn = tkinter.Button(text="Reset")
+reset_btn.grid(column=3,row=2)
+
+check_label = tkinter.Label(text=TICK,fg=GREEN,bg=YELLOW,font=(FONT_NAME,30,"normal"))
+check_label.grid(column=1,row=3)
+
+
 
 
 
