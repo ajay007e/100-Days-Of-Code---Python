597f4605ec5faec0885a26299de3dab0b2a3aa0e
Ajay Mathew <ajayedathazhe@gmail.com>
2022-10-18 20:25:07 +0530

    day 30 exercised added, nato phenotic alphabet project edited

--- day 026/NATO-alphabet/main.py
+++ day 026/NATO-alphabet/main.py
@@ -1,9 +1,24 @@
+# import pandas as pd
+# #TODO 1. Create a dictionary in this format:
+# data = pd.read_csv("day 026/NATO-alphabet/nato_phonetic_alphabet.csv")
+# nato_alphabets = {word["letter"]:word["code"] for (index,word) in data.iterrows()}
+
+# #TODO 2. Create a list of the phonetic code words from a word that the user inputs.
+# user_input = input("Please enter the word you want to code:")
+# code_list = [nato_alphabets[letter.upper()] for letter in user_input]
+# print(code_list)
+
+#TODO updatting the code in such a way that only valid letters are accepting
+
 import pandas as pd
-#TODO 1. Create a dictionary in this format:
 data = pd.read_csv("day 026/NATO-alphabet/nato_phonetic_alphabet.csv")
 nato_alphabets = {word["letter"]:word["code"] for (index,word) in data.iterrows()}
-
-#TODO 2. Create a list of the phonetic code words from a word that the user inputs.
-user_input = input("Please enter the word you want to code:")
-code_list = [nato_alphabets[letter.upper()] for letter in user_input]
+# code_list = None
+while True:
+    user_input = input("Please enter the word you want to code:")
+    try:
+        code_list = [nato_alphabets[letter.upper()] for letter in user_input]
+        break
+    except KeyError:
+        print("Sorry, Only letters in the alphabets are allowed")
 print(code_list)
--- day 030/main.py
+++ day 030/main.py
@@ -28,3 +28,40 @@ if height > 3 : raise ValueError("Height must be less than or equal to 3")
 
 print(f"BMI is {weight/(height**2)}")
 
+
+#TODO 3 - exercise 1
+
+fruits = ["Apple", "Pear", "Orange"]
+
+def make_pie(index):
+    try:
+        fruit = fruits[index]
+    except IndexError:
+        print("Fruit pie")
+    else:
+        print(fruit + " pie")
+
+make_pie(3)
+
+#TODO 4 - exercise 2
+
+facebook_posts = [
+    {'Likes': 21, 'Comments': 2}, 
+    {'Likes': 13, 'Comments': 2, 'Shares': 1}, 
+    {'Likes': 33, 'Comments': 8, 'Shares': 3}, 
+    {'Comments': 4, 'Shares': 2}, 
+    {'Comments': 1, 'Shares': 1}, 
+    {'Likes': 19, 'Comments': 3}
+]
+
+total_likes = 0
+
+for post in facebook_posts:
+    try:
+        post["Likes"]
+    except:
+        pass
+    else:
+        total_likes = total_likes + post['Likes']
+
+print(total_likes)
