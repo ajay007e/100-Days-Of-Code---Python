003f43b18f25d561862191f2e30193b522b3f022
Ajay Mathew <ajayedathazhe@gmail.com>
2022-10-18 20:14:45 +0530

    day 30 user defined errors

--- day 030/main.py
+++ day 030/main.py
@@ -1,4 +1,5 @@
-# TODO 1 - familarize with exception handling
+
+#TODO 1 - familarize with exception handling
 
 try:
     file  = open("day 030/text.txt")
@@ -15,4 +16,15 @@ else:
 finally:
     file.close()
     print("file closed successfully")
+    # raise TypeError("This is the error that i made up.!")
+
+
+#TODO 2 - user defined errors
+
+height  = float(input("Height:"))
+weight = int(input("Weight:"))
+
+if height > 3 : raise ValueError("Height must be less than or equal to 3")
+
+print(f"BMI is {weight/(height**2)}")
 
