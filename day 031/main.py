454a71bee92b20d4a047c8f5206164729db0de89
Ajay Mathew <ajayedathazhe@gmail.com>
2022-10-26 19:12:51 +0530

    day 31 functionality started

--- day 031/main.py
+++ day 031/main.py
@@ -1,7 +1,24 @@
 import tkinter
 import pandas as pd
+import random
 
 BACKGROUND_COLOR = "#B1DDC6"
+
+data = pd.read_csv("day 031/data/french_words.csv")
+dic = {}
+for (index,row) in data.iterrows():
+    dic[index] = [row.English,row.French]
+
+def get_word():
+    return dic[random.randint(0,len(dic))]
+
+def cross_btn_click():
+    word = get_word()
+    canvas.itemconfig(lang_text,text=word[1])
+
+def tick_btn_click():
+    word = get_word()
+    canvas.itemconfig(lang_text,text=word[1])
 
 # ---------------------------- UI SETUP ------------------------------- #
 
@@ -16,19 +33,19 @@ back_img = tkinter.PhotoImage(file="day 031/images/card_back.png")
 canvas.create_image(400,263,image=front_img)
 canvas.grid(column=0,row=0,columnspan=2)
 
+word = get_word()
 
-
-french_text = canvas.create_text(400,150,text="Title",font=("Ariel",40,"italic"))
-english_text = canvas.create_text(400,263,text="Word",font=("Arial",60,"bold"))
+title_text = canvas.create_text(400,150,text="French",font=("Ariel",40,"italic"))
+lang_text = canvas.create_text(400,263,text=word[1],font=("Arial",60,"bold"))
 
 wrong_img = tkinter.PhotoImage(file="day 031/images/wrong.png")
 right_img = tkinter.PhotoImage(file="day 031/images/right.png")
 
 
-wrong_btn = tkinter.Button(image=wrong_img,highlightthickness=0)
+wrong_btn = tkinter.Button(image=wrong_img,highlightthickness=0,command=cross_btn_click)
 wrong_btn.grid(column=0,row=1)
 
-right_btn = tkinter.Button(image=right_img,highlightthickness=0)
+right_btn = tkinter.Button(image=right_img,highlightthickness=0,command=tick_btn_click)
 right_btn.grid(column=1,row=1)
 
 
